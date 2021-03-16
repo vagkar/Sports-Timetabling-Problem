40169788f62973213abb594653fb20f0db1e4825
Evangelos Karampoulis <vaggos971@gmail.com>
2021-03-17 00:43:57 +0200

    read from XML Separation Constraint

--- src/com/thesis/constraints/separation/SeparationConstraints.java
+++
@@ -1,8 +0,0 @@
-package com.thesis.constraints.separation;
-
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name = "SeparationConstraints")
-public class SeparationConstraints {
-
-}
