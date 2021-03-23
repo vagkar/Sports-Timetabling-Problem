68b37c98dcfe03fc096cbfe9f98d1357704b4c87
Evangelos Karampoulis <vaggos971@gmail.com>
2021-03-23 21:31:21 +0200

    try exception for null constraint

--- src/com/thesis/Main.java
+++ src/com/thesis/Main.java
@@ -1,5 +1,7 @@
 package com.thesis;
 
+import com.thesis.constraints.separation.SE1;
+import com.thesis.resources.League;
 import com.thesis.resources.Slot;
 import com.thesis.resources.Team;
 
@@ -10,17 +12,35 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws JAXBException {
-        File file = new File("instances/test.xml");
+        File file = new File("instances/ITC2021_Test6.xml");
         JAXBContext jaxbContext = JAXBContext.newInstance(Instance.class);
 
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
         Instance instance = (Instance) jaxbUnmarshaller.unmarshal(file);
+
         for (Team t : instance.getResources().getTeams())
             System.out.println(t.getId() + " " + t.getName() + " " + t.getLeague());
-        System.out.println(instance.getResources().getLeague().get(0).getName());
+
+        System.out.println("==================================");
+
+        League l = instance.getResources().getLeague().get(0);
+        System.out.println(l.getId() + " " + l.getName());
+
+        System.out.println("==================================");
+
         for (Slot s : instance.getResources().getSlots())
             System.out.println(s.getId() + " " + s.getName());
 
-        System.out.println(instance.getConstraints().getSE1().get(0).getTeams());
+        System.out.println("==================================");
+
+        SE1 se1;
+        try {
+            se1 = instance.getConstraints().getSE1().get(0);
+            System.out.println(se1.getMode1() + " " + se1.getMin() + " " + se1.getTeams()
+                    + " " + se1.getType());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("No SE1 Constraint!");
+        }
+
     }
 }
