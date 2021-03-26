47f8c42b2f8fbe0f8937d14b76455e827cd26cde
Evangelos Karampoulis <vaggos971@gmail.com>
2021-03-26 23:28:33 +0200

    create Class ParseXML

--- src/com/thesis/Main.java
+++ src/com/thesis/Main.java
@@ -5,18 +5,12 @@ import com.thesis.resources.League;
 import com.thesis.resources.Slot;
 import com.thesis.resources.Team;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws JAXBException {
-        File file = new File("instances/ITC2021_Test6.xml");
-        JAXBContext jaxbContext = JAXBContext.newInstance(Instance.class);
-
-        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-        Instance instance = (Instance) jaxbUnmarshaller.unmarshal(file);
+        ParseXML parseXML = new ParseXML();
+        Instance instance = parseXML.getInstance();
 
         for (Team t : instance.getResources().getTeams())
             System.out.println(t.getId() + " " + t.getName() + " " + t.getLeague());
---
+++ src/com/thesis/ParseXML.java
@@ -0,0 +1,28 @@
+package com.thesis;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
+public class ParseXML {
+
+    private Instance instance;
+
+    public ParseXML() throws JAXBException {
+        File file = new File("instances/ITC2021_Test6.xml");
+        JAXBContext jaxbContext = JAXBContext.newInstance(Instance.class);
+
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        Instance instance = (Instance) jaxbUnmarshaller.unmarshal(file);
+        setInstance(instance);
+    }
+
+    public Instance getInstance() {
+        return instance;
+    }
+
+    public void setInstance(Instance instance) {
+        this.instance = instance;
+    }
+}
