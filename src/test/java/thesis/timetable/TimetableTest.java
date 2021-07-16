5141817f8542acdd043d8f684634f6806aed204d
Evangelos Karampoulis <vaggos971@gmail.com>
2021-07-16 17:51:48 +0300

    Created TimetableTest Class

---
+++ src/test/java/thesis/timetable/TimetableTest.java
@@ -0,0 +1,48 @@
+package thesis.timetable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimetableTest {
+
+    @Test
+    void CA1Penalty() {
+    }
+
+    @Test
+    void CA2Penalty() {
+    }
+
+    @Test
+    void CA3Penalty() {
+    }
+
+    @Test
+    void CA4Penalty() {
+    }
+
+    @Test
+    void GA1Penalty() {
+    }
+
+    @Test
+    void BR1Penalty() {
+    }
+
+    @Test
+    void BR2Penalty() {
+    }
+
+    @Test
+    void FA2Penalty() {
+    }
+
+    @Test
+    void SE1Penalty() {
+    }
+
+    @Test
+    void computePenalties() {
+    }
+}
