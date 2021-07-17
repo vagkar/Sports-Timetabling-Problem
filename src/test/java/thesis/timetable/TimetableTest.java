37b2d68e13dfc93806a590d7ed75593d5a43cdb4
Evangelos Karampoulis <vaggos971@gmail.com>
2021-07-17 13:11:43 +0300

    All penalty tests created

--- src/test/java/thesis/timetable/TimetableTest.java
+++ src/test/java/thesis/timetable/TimetableTest.java
@@ -37,37 +37,64 @@ class TimetableTest {
 
     @Test
     void CA2PenaltyTest() {
+        ObjectiveValue objectiveValue = timetable.CA2Penalty(instance.getConstraints().getCA2());
+        Assertions.assertEquals(6, objectiveValue.getInfeasibility());
+        Assertions.assertEquals(16, objectiveValue.getObjective());
     }
 
     @Test
     void CA3PenaltyTest() {
+        ObjectiveValue objectiveValue = timetable.CA3Penalty(instance.getConstraints().getCA3(), instance.getResources().getSlots().size());
+        Assertions.assertEquals(8, objectiveValue.getInfeasibility());
+        Assertions.assertEquals(330, objectiveValue.getObjective());
     }
 
     @Test
     void CA4PenaltyTest() {
+        ObjectiveValue objectiveValue = timetable.CA4Penalty(instance.getConstraints().getCA4());
+        Assertions.assertEquals(6, objectiveValue.getInfeasibility());
+        Assertions.assertEquals(16, objectiveValue.getObjective());
     }
 
     @Test
     void GA1PenaltyTest() {
+        ObjectiveValue objectiveValue = timetable.GA1Penalty(instance.getConstraints().getGa1Constraints());
+        Assertions.assertEquals(0, objectiveValue.getInfeasibility());
+        Assertions.assertEquals(4, objectiveValue.getObjective());
     }
 
     @Test
     void BR1PenaltyTest() {
+        ObjectiveValue objectiveValue = timetable.BR1Penalty(instance.getConstraints().getBR1());
+        Assertions.assertEquals(6, objectiveValue.getInfeasibility());
+        Assertions.assertEquals(16, objectiveValue.getObjective());
     }
 
     @Test
     void BR2PenaltyTest() {
+        ObjectiveValue objectiveValue = timetable.BR2Penalty(instance.getConstraints().getBR2());
+        Assertions.assertEquals(14, objectiveValue.getInfeasibility());
+        Assertions.assertEquals(0, objectiveValue.getObjective());
     }
 
     @Test
     void FA2PenaltyTest() {
+        ObjectiveValue objectiveValue = timetable.FA2Penalty(instance.getConstraints().getFa2Constraints());
+        Assertions.assertEquals(0, objectiveValue.getInfeasibility());
+        Assertions.assertEquals(0, objectiveValue.getObjective());
     }
 
     @Test
     void SE1PenaltyTest() {
+        ObjectiveValue objectiveValue = timetable.SE1Penalty(instance.getConstraints().getSE1());
+        Assertions.assertEquals(0, objectiveValue.getInfeasibility());
+        Assertions.assertEquals(900, objectiveValue.getObjective());
     }
 
     @Test
     void computePenaltiesTest() {
+        ObjectiveValue objectiveValue = timetable.computePenalties(instance);
+        Assertions.assertEquals(28, objectiveValue.getInfeasibility());
+        Assertions.assertEquals(1250, objectiveValue.getObjective());
     }
 }
